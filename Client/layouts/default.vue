eb606f9640a3c12c33867b4d33c9a8ac3c7ea018
Mathias Odllund <mathiasodlund@proton.me>
2023-03-23 18:26:09 +0100

    init

---
+++ Client/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <Navbar />
+<div class="h-[90vh]">
+    <slot />
+</div>
+  <Footer />
+</template>
---
+++ Client/pages/product/[slug].vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Page: {{$route.params.slug}}
+  </div>
+</template>
+
+<style scoped></style>
